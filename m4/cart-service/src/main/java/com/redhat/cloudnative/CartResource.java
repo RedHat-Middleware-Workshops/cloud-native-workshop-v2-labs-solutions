d5be720cb9955e2f3318b7d86cbb340a55acbebf
danieloh30 <doh@redhat.com>
2021-01-25 19:26:31 -0500

    update annotation based serialization

--- m4/cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
+++ m4/cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
@@ -4,7 +4,6 @@ import com.redhat.cloudnative.model.order.Order;
 import com.redhat.cloudnative.model.ShoppingCart;
 import com.redhat.cloudnative.service.ShoppingCartService;
 import io.vertx.core.json.Json;
-import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +12,10 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+// import org.apache.kafka.clients.producer.KafkaProducer;
+// import org.apache.kafka.clients.producer.Producer;
+// import org.apache.kafka.clients.producer.ProducerRecord;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.apache.kafka.common.header.internals.RecordHeaders;
 
 import io.quarkus.runtime.StartupEvent;
 
@@ -30,36 +28,15 @@ public class CartResource {
     private static final Logger log = LoggerFactory.getLogger(CartResource.class);
 
     // TODO: Add annotation of orders messaging configuration here
-    @ConfigProperty(name = "mp.messaging.outgoing.orders.bootstrap.servers")
-    public String bootstrapServers;
-
-    @ConfigProperty(name = "mp.messaging.outgoing.orders.topic")
-    public String ordersTopic;
-
-    @ConfigProperty(name = "mp.messaging.outgoing.orders.value.serializer")
-    public String ordersTopicValueSerializer;
-
-    @ConfigProperty(name = "mp.messaging.outgoing.orders.key.serializer")
-    public String ordersTopicKeySerializer;
-
-    private Producer<String, String> producer;
-
+    
     @Inject
     ShoppingCartService shoppingCartService;
 
     // TODO ADD getCart method
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/{cartId}")
-    @Operation(summary = "get the contents of cart by cartId")
-    public ShoppingCart getCart(@PathParam("cartId") String cartId) {
-        return shoppingCartService.getShoppingCart(cartId);
-    }
 
     @POST
-    @Path("/{cartId}/{itemId}/{quantity}")
+    @Path("{cartId}/{itemId}/{quantity}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Add an Item with its quantity")
     public ShoppingCart add(@PathParam("cartId") String cartId,
                             @PathParam("itemId") String itemId,
                             @PathParam("quantity") int quantity) throws Exception {
@@ -67,18 +44,16 @@ public class CartResource {
     }
 
     @POST
-    @Path("/{cartId}/{tmpId}")
+    @Path("{cartId}/{tmpId}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Set the cart with a new Id")
     public ShoppingCart set(@PathParam("cartId") String cartId,
                             @PathParam("tmpId") String tmpId) throws Exception {
         return shoppingCartService.set(cartId, tmpId);
     }
 
     @DELETE
-    @Path("/{cartId}/{itemId}/{quantity}")
+    @Path("{cartId}/{itemId}/{quantity}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "delete an the quantity or item from the cart")
     public ShoppingCart delete(@PathParam("cartId") String cartId,
                                @PathParam("itemId") String itemId,
                                @PathParam("quantity") int quantity) throws Exception {
@@ -86,31 +61,23 @@ public class CartResource {
     }
 
     @POST
-    @Path("/checkout/{cartId}")
+    @Path("checkout/{cartId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "checkout")
     public ShoppingCart checkout(@PathParam("cartId") String cartId, Order order) {
-        sendOrder(order, cartId);
+        // TODO ADD for KAFKA
+        //sendOrder(order, cartId);
         return shoppingCartService.checkout(cartId);
     }
 
     // TODO ADD for KAFKA
     private void sendOrder(Order order, String cartId) {
-        order.setTotal(shoppingCartService.getShoppingCart(cartId).getCartTotal() + "");
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(ordersTopic, null, null, null, Json.encode(order), new RecordHeaders().add("content-type", "application/json".getBytes()));
-        producer.send(producerRecord);
-        log.info("Sent message: " + Json.encode(order));
+
     }
 
     // TODO ADD for KAFKA
     public void init(@Observes StartupEvent ev) {
-        Properties props = new Properties();
 
-        props.put("bootstrap.servers", bootstrapServers);
-        props.put("value.serializer", ordersTopicValueSerializer);
-        props.put("key.serializer", ordersTopicKeySerializer);
-        producer = new KafkaProducer<String, String>(props);
     }
 
 
