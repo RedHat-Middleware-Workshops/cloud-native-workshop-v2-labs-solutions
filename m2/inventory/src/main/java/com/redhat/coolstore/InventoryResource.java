4d21bfc82cf89c67c72c904f4c8e74ccfc665279
Daniel Oh <doh@redhat.com>
2023-10-24 11:08:50 -0400

    Update reactive jackson

--- m2/inventory/src/main/java/com/redhat/coolstore/InventoryResource.java
+++ m2/inventory/src/main/java/com/redhat/coolstore/InventoryResource.java
@@ -8,14 +8,13 @@ import jakarta.json.Json;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/services/inventory")
 @ApplicationScoped
